73197f1b2d7bfb5b9e9c4ceb778817999c0e2683
Christoph Gruener <christophgruener@me.com>
2019-02-19 23:54:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays_with_BBQ = []
     holiday.each do |day, supplies|
       if supplies.include?("BBQ")
-        holidays_with_BBQ << holiday[day]
+        holidays_with_BBQ << holiday.keys
       end
     end
     holidays_with_BBQ
