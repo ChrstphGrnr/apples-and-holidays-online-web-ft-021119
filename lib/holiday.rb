c063f66775d95901369fea179bd8016c404f984a
Christoph Gruener <christophgruener@me.com>
2019-02-19 22:52:53 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,6 +24,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
+  end
+
+
 
 end
 
@@ -61,10 +66,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
